34541bbe0d202ebc6eef7fb36f28825737fa5a24
AlbertoMApps <alberto.medrano.theappexperts@gmail.com>
2016-01-26 09:15:05 +0000

    new favouriteslist, maps functionality and design

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# finalProject
+# finalProject:
+App name: Buen provecho
+Description: Spanish restaurant finder in the world.
+Purpose: To find new places near you live within the map and search for new places within your city and see the details and different features.
--- app/src/main/java/com/example/tae_user0/finalprojectapp1/Fragments/FragmentPackages/FragmentPart2/FragmentMaps.java
+++ app/src/main/java/com/example/tae_user0/finalprojectapp1/Fragments/FragmentPackages/FragmentPart2/FragmentMaps.java
@@ -1,41 +1,33 @@
 package com.example.tae_user0.finalprojectapp1.Fragments.FragmentPackages.FragmentPart2;
 
 
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.tae_user0.finalprojectapp1.Api.CitiesApi;
-import com.example.tae_user0.finalprojectapp1.Api.RestaurantsApiAllCitiesMaps;
+import com.example.tae_user0.finalprojectapp1.Api.RestaurantsApi;
 import com.example.tae_user0.finalprojectapp1.Constants.Constant;
 import com.example.tae_user0.finalprojectapp1.Model.api2.Restaurant;
 import com.example.tae_user0.finalprojectapp1.Model.api2.SpanishRestaurantModel;
-import com.example.tae_user0.finalprojectapp1.Model.api4.CoordinatesModel;
 import com.example.tae_user0.finalprojectapp1.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.squareup.picasso.Picasso;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import retrofit.Callback;
@@ -47,10 +39,13 @@ import retrofit.client.Response;
 /**
  * Created by TAE_user0 on 25/01/2016.
  */
-public class FragmentMaps extends Fragment implements OnMapReadyCallback {
+public class FragmentMaps extends Fragment implements OnMapReadyCallback, AdapterView.OnItemSelectedListener{
     private RestAdapter restAdapt;
     private GoogleMap mMap;
-    private RestaurantsApiAllCitiesMaps spAllCities;
+    private RestaurantsApi spAllCities;
+    private Spinner spinner;
+    private String itemSpinner;
+    private int cityNumber;
 
     @Nullable
     @Override
@@ -58,6 +53,14 @@ public class FragmentMaps extends Fragment implements OnMapReadyCallback {
         View v = inflater.inflate(R.layout.fragment_maps, container, false);
         //mapview
         initilizeMap();
+        //init the rest of the view elements here---
+        TextView txtLabel = (TextView) v.findViewById(R.id.txtMaps);
+        spinner = (Spinner) v.findViewById(R.id.spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity().getApplicationContext(), R.array.citiesMaps, R.layout.support_simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
         //restAdapter of the models
         restAdapt = new RestAdapter.Builder()
                 .setEndpoint(Constant.BASE_URL)
@@ -72,29 +75,33 @@ public class FragmentMaps extends Fragment implements OnMapReadyCallback {
         mMap = googleMap;
         mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
         //get retrofit options for the new api to get all the restaurants around
-        spAllCities = restAdapt.create(RestaurantsApiAllCitiesMaps.class);
-        spAllCities.getAllSpanishRestaurants(new Callback<SpanishRestaurantModel>() {
-            @Override
-            public void success(SpanishRestaurantModel spanishRestaurantModel, Response response) {
-                List<Restaurant> restaurantsList = spanishRestaurantModel.getRestaurants();
-                for (int i = 0; i < restaurantsList.size(); i++) {
-                    double lat = Double.parseDouble(restaurantsList.get(i).getRestaurant().getLocation().getLatitude());
-                    double lng = Double.parseDouble(restaurantsList.get(i).getRestaurant().getLocation().getLongitude());
-                    final LatLng place = new LatLng(lat, lng);
-                    MarkerOptions placeMarker = new MarkerOptions()
-                            .position(place)
-                            .title(restaurantsList.get(i).getRestaurant().getName())
-                            .snippet("Scope: " + restaurantsList.get(i).getRestaurant().getLocation().getAddress());
-                    mMap.addMarker(placeMarker);
-                    //                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(place, 12.5f));
+        spAllCities = restAdapt.create(RestaurantsApi.class);
+        if (getCityNumber()!=0) {
+            spAllCities.getSpanishRestaurants(getCityNumber(), new Callback<SpanishRestaurantModel>() {
+                @Override
+                public void success(SpanishRestaurantModel spanishRestaurantModel, Response response) {
+                    List<Restaurant> restaurantsList = spanishRestaurantModel.getRestaurants();
+                    for (int i = 0; i < restaurantsList.size(); i++) {
+                        double lat = Double.parseDouble(restaurantsList.get(i).getRestaurant().getLocation().getLatitude());
+                        double lng = Double.parseDouble(restaurantsList.get(i).getRestaurant().getLocation().getLongitude());
+                        final LatLng place = new LatLng(lat, lng);
+                        MarkerOptions placeMarker = new MarkerOptions()
+                                .position(place)
+                                .title(restaurantsList.get(i).getRestaurant().getName())
+                                .snippet("Address: " + restaurantsList.get(i).getRestaurant().getLocation().getAddress());
+                        mMap.addMarker(placeMarker);
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(place, 12.5f));
+                    }
                 }
-            }
 
-            @Override
-            public void failure(RetrofitError error) {
-                getToast("failure loading retrofit for the restaurant places");
-            }
-        });
+                @Override
+                public void failure(RetrofitError error) {
+
+                }
+            });
+        } else {
+            this.cityNumber = 61;
+        }
         // Enable / Disable zooming controls
         mMap.getUiSettings().setZoomControlsEnabled(false);
 
@@ -110,7 +117,7 @@ public class FragmentMaps extends Fragment implements OnMapReadyCallback {
         // Enable / Disable zooming functionality
         mMap.getUiSettings().setZoomGesturesEnabled(true);
 
-        //My location
+        //My location by default
         double lat = 51.510210;
         double lng = -0.070880;
         final LatLng place = new LatLng(lat, lng);
@@ -119,7 +126,7 @@ public class FragmentMaps extends Fragment implements OnMapReadyCallback {
                 .title("My position")
                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
         mMap.addMarker(placeMarker);
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(place, 12.5f));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(place, 12.5f));
     }
 
     //methods
@@ -157,5 +164,30 @@ public class FragmentMaps extends Fragment implements OnMapReadyCallback {
             Log.i("MainActivity", "nothing on backstack, calling super");
             //super.onBackPressed();
         }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Log.i("MAIN", "spinner " + position);
+        itemSpinner = parent.getItemAtPosition(position).toString();
+       // getToast(itemSpinner);
+        if( position==1) {
+            this.cityNumber=68;
+        } else if( position==2 ){
+            this.cityNumber =295;
+        } else if ( position==3 ){
+            this.cityNumber =283;
+        }else {
+            this.cityNumber =61;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        getToast("Select an item");
+    }
+    //methods
+    public int getCityNumber(){
+        return this.cityNumber;
     }
 }
